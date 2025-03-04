889601e76c558311bd43495f1c897e0f80e4a9a9
XZH-jesse <134376344+XZH-jesse@users.noreply.github.com>
2025-03-04 15:36:32 +0800

    done with project 1 EC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -13,33 +13,33 @@ public class TestArrayDequeEC {
         ArrayDequeSolution<Integer> correct = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> broken = new StudentArrayDeque<>();
         StringBuilder message = new StringBuilder();
-        int N = 500;
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
             int opRandom = StdRandom.uniform(0, 4);
             if (opRandom == 0) {
                 //addFirst
-                int randVal = StdRandom.uniform(0, 10000);
+                int randVal = StdRandom.uniform(0, 100);
                 correct.addFirst(randVal);
                 broken.addFirst(randVal);
                 message.append("addFirst(" + randVal + ")\n");
             } else if (opRandom == 1) {
                 //addLast
-                int randVal = StdRandom.uniform(0, 10000);
+                int randVal = StdRandom.uniform(0, 100);
                 correct.addLast(randVal);
                 broken.addLast(randVal);
                 message.append("addLast(" + randVal + ")\n");
             } else if (opRandom == 2) {
                 //removeFirst
                 if (!correct.isEmpty() && !broken.isEmpty()) {
-                    int valCorrect = correct.removeFirst();
-                    int valBroken = broken.removeFirst();
+                    Integer valCorrect = correct.removeFirst();
+                    Integer valBroken = broken.removeFirst();
                     message.append("removeFirst()\n");
                     assertEquals(message.toString(), valCorrect, valBroken);
                 }
-            } else {
+            } else if (opRandom == 3) {
                 if (!correct.isEmpty() && !broken.isEmpty()) {
-                    int valCorrect = correct.removeLast();
-                    int valBroken = broken.removeLast();
+                    Integer valCorrect = correct.removeLast();
+                    Integer valBroken = broken.removeLast();
                     message.append("removeLast()\n");
                     assertEquals(message.toString(), valCorrect, valBroken);
                 }
